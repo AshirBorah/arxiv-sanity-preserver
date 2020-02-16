1af113002a311faf010d05610da6ceaf114dc2ad
Ashir Borah <aborah@broadinstitute.org>
2020-02-16 17:42:19 -0500

    Added multithreaded

--- download_pdfs.py
+++ download_pdfs.py
@@ -4,21 +4,15 @@ import pickle
 import shutil
 import random
 from  urllib.request import urlopen
+from multiprocessing.pool import ThreadPool
+import threading
+import queue
 
 from utils import Config
 
-timeout_secs = 10 # after this many seconds we give up on a paper
-if not os.path.exists(Config.pdf_dir): os.makedirs(Config.pdf_dir)
-have = set(os.listdir(Config.pdf_dir)) # get list of all pdfs we already have
-
-numok = 0
-numtot = 0
-db = pickle.load(open(Config.db_path, 'rb'))
-for pid,j in db.items():
-    download_pdf(pid,j)
-
-
 def download_pdf(pid,j):
+  global numtot
+  global numok
   pdf_url = j['links']
   print(pdf_url)
   #assert len(pdfs) == 1
@@ -41,7 +35,45 @@ def download_pdf(pid,j):
   except Exception as e:
     print('error downloading: ', pdf_url)
     print(e)
+  return pdf_url
 
   print('%d/%d of %d downloaded ok.' % (numok, numtot, len(db)))
+
+timeout_secs = 10 # after this many seconds we give up on a paper
+if not os.path.exists(Config.pdf_dir): os.makedirs(Config.pdf_dir)
+have = set(os.listdir(Config.pdf_dir)) # get list of all pdfs we already have
+
+numok = 0
+numtot = 0
+db = pickle.load(open(Config.db_path, 'rb'))
+# for pid,j in db.items():
+#     threading.Thread(target=download_pdf, args=(pid, j)).start()
+
+def worker():
+    while True:
+        item= q.get()
+        download_pdf(item[0], item[1])
+        q.task_done()
+
+num_worker_threads = 8
+
+q = queue.Queue()
+for i in range(num_worker_threads):
+     t = threading.Thread(target=worker)
+     t.daemon = True
+     t.start()
+
+for pid,j in db.items():
+    q.put((pid,j))
+
+q.join()
+
+#
+# for pid,j in db.items():
+#     ThreadPool(8).imap_unordered(download_pdf, db.items())
+#     download_pdf(pid,j)
+
+
+
 
 print('final number of papers downloaded okay: %d/%d' % (numok, len(db)))

a9e495a39b8aa5b72d9a6169d6de1e82970df350
EagleShot <ashiraseesh@gmail.com>
2020-03-30 20:15:12 -0400

    Updated run script

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-python fetch_papers.py --start_date 2012-01-01 --end_date 2020-03-19
-# python download_pdfs.py
-# python parse_pdf_to_text.py
-# python thumb_pdf.py
-# python analyze.py
-# python buildsvm.py
-# python make_cache.py
+python fetch_papers.py --start_date 2012-01-01 --end_date 2020-04-01 --wait-time 3
+python download_pdfs.py
+python parse_pdf_to_text.py
+python thumb_pdf.py
+python analyze.py
+python buildsvm.py
+python make_cache.py
